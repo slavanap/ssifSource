5e9f49da3be3948592063a4308a77343dadcd5ed
Vyacheslav Napadovsky <napadovskiy@gmail.com>
2013-05-27 21:36:39 +0400

    ssifSource3 twice load issue fixed. Info messages added.

--- several_files.cpp
+++ several_files.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
 #include "several_files.h"
 
+#define SSIFSOURCE3_PLUGIN "ssifSource3"
+#define FORMAT_PRINTMESSAGE(x) SSIFSOURCE3_PLUGIN ": " x "\n"
+
 SSIFSourceExt::SSIFSourceExt(AVSValue& args, IScriptEnvironment* env): cfile(NULL), env(env) {
 	cfile_idx = -1;
 	bLeft = args[1].AsBool(false);
@@ -39,11 +42,18 @@ void SSIFSourceExt::AddFileNext(const string& name, int frames, bool load) {
 	files_names.push_back(name);
 	if (frames <= 0) 
 		load = true;
+	printf(FORMAT_PRINTMESSAGE("adding file %s with %d frames to sequences list. Have to load flag is %s"), 
+		name.c_str(), frames, load ? "TRUE" : "FALSE");
 	if (load) {
 		AVSValue args[7] = {name.c_str(), frames, bLeft, bRight, bHorizontalStack, iSwapViews, bCreateIndex};
 		SSIFSource *obj = new SSIFSource(AVSValue(args, sizeof(args)/sizeof(AVSValue)), env);
 		frames = obj->GetVideoInfo().num_frames;
-		delete obj;
+		if (cfile_idx < 0) {
+			cfileclip_holder = cfile = obj;
+			cfile_idx = files_names.size()-1;
+		}
+		else
+			delete obj;
 	}
 	framenum_offsets.push_back(frames + *framenum_offsets.rbegin());
 	vi.num_frames = *framenum_offsets.rbegin();
