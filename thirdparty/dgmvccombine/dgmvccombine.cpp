feb1305bc3f03c7e5e1990aa896d40be2eed48b9
Vyacheslav Napadovsky <napadovskiy@gmail.com>
2014-06-22 03:31:03 +0400

    Another update by neuron2 (changes description wasn't published). Mostly code simplification.

--- thirdparty/dgmvccombine/dgmvccombine.cpp
+++ thirdparty/dgmvccombine/dgmvccombine.cpp
@@ -17,8 +17,6 @@ unsigned char dependent_buffer[BUF_SIZE];
 unsigned char *base_au, *dependent_au;
 int base_au_size, dependent_au_size;
 FILE *base_fp, *dependent_fp, *combined_fp;
-int base_has_9, base_has_7, base_has_8, base_has_6, base_has_5, base_has_1;
-int dependent_has_6, dependent_has_8, dependent_has_15, dependent_has_20;
 
 int get_next_au(unsigned char *buf, FILE *fp)
 {
@@ -42,7 +40,10 @@ int get_next_au(unsigned char *buf, FILE *fp)
 	{
 		p++;
 		if (p >= buf + read)
+		{
 			hit_eof = 1;
+			break;
+		}
 		if (p[-4] == 0 && p[-3] == 0 && p[-2] == 1 && (p[-1] & 0x1f) == (buf == base_buffer ? 9 : 24))
 			break;
 	}
@@ -58,67 +59,6 @@ int get_next_au(unsigned char *buf, FILE *fp)
 	return (hit_eof ? 1 : 0);
 }
 
-void write_all(unsigned char *buf, int type)
-{
-	unsigned char *p = buf + 3;
-	unsigned char *start, *end;
-	unsigned char c;
-
-	while (1)
-	{
-		while (1)
-		{
-			p++;
-			if (p[-4] == 0 && p[-3] == 0 && p[-2] == 1)
-			{
-				c = (p[-1] & 0x1f);
-				if (buf == base_buffer)
-				{
-					if (c == 7)
-						base_has_7 = 1;
-					else if (c == 8)
-						base_has_8 = 1;
-					else if (c == 6)
-						base_has_6 = 1;
-					else if (c == 5)
-						base_has_5 = 1;
-					else if (c == 1)
-						base_has_1 = 1;
-				}
-				else
-				{
-					if (c == 6)
-						dependent_has_6 = 1;
-					else if (c == 8)
-						dependent_has_8 = 1;
-					else if (c == 20)
-						dependent_has_20 = 1;
-				}
-				if (c == type)
-					break;
-			}
-			if (p >= buf + (buf == base_buffer ? base_au_size : dependent_au_size))
-				return;
-		}
-		start = p - 4;
-		while (1)
-		{
-			p++;
-			if (p >= buf + (buf == base_buffer ? base_au_size : dependent_au_size))
-			{
-				end = p;
-				break;
-			}
-			if (p[-3] == 0 && p[-2] == 0 && p[-1] == 1)
-			{
-				end = p - 3;
-				break;
-			}
-		}
-		fwrite(start, 1, end - start, combined_fp);
-	}
-}
-
 int main(int argc, char **argv)
 {
 	int count;
@@ -126,7 +66,7 @@ int main(int argc, char **argv)
 
 	if (argc < 4)
 	{
-		printf("DGMVCCombine 1.0.1 by Donald A. Graft, Copyright (C) 2014, All Rights Reserved\n");
+		printf("DGMVCCombine 1.0.2 by Donald A. Graft, Copyright (C) 2014, All Rights Reserved\n");
 		printf("usage: dgmvccombine base dependent combined\n");
 		return 1;
 	}
@@ -157,35 +97,16 @@ int main(int argc, char **argv)
 	// Loop through the access units.
 	while (1)
 	{
-		base_has_9 = base_has_7 = base_has_8 = base_has_6 = base_has_5 = base_has_1 = 0;
-		dependent_has_6 = dependent_has_8 = dependent_has_15 = dependent_has_20 = 0;
 		status = get_next_au(base_buffer, base_fp);
 		status |= get_next_au(dependent_buffer, dependent_fp);
 		count++;
-//		if (!(count % 100)) printf("%d frames processed\r", count);
-		printf("%d frames processed\n", count);
-		write_all(base_buffer, 9);
-		if (base_has_7)
-			write_all(base_buffer, 7);
-		write_all(dependent_buffer, 15);
-		if (base_has_8)
-			write_all(base_buffer, 8);
-		if (dependent_has_8)
-			write_all(dependent_buffer, 8);
-		if (base_has_6)
-			write_all(base_buffer, 6);
-		if (base_has_5)
-			write_all(base_buffer, 5);
-		if (base_has_1)
-			write_all(base_buffer, 1);
-		if (dependent_has_6)
-			write_all(dependent_buffer, 6);
-		if (dependent_has_20)
-			write_all(dependent_buffer, 20);
+		if (!(count % 50))
+			printf("%d frames processed\r", count);
+		fwrite(base_au, 1, base_au_size, combined_fp);
+		fwrite(dependent_au, 1, dependent_au_size, combined_fp);
 		if (status == 1)
 			break;
 	}
-
 	fclose(base_fp);
 	fclose(dependent_fp);
 	fclose(combined_fp);
