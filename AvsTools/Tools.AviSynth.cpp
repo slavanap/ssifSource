d426f9a54328bbc0463368ce6fdd0a3e09e71308
Vyacheslav Napadovsky <napadovskiy@gmail.com>
2016-08-27 05:03:54 +0300

    copyright notice fix, reference optimizations

--- AvsTools/Tools.AviSynth.cpp
+++ AvsTools/Tools.AviSynth.cpp
@@ -1,23 +1,25 @@
+//
 // AviSynth useful C++ wrappers
 // Copyright 2016 Vyacheslav Napadovsky.
-//
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files(the "Software"), to deal
+// of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions :
-//
+// furnished to do so, subject to the following conditions:
+// 
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
 // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 
 #include "stdafx.h"
 #include "Tools.AviSynth.hpp"
@@ -41,12 +43,9 @@ namespace Tools {
 		}
 
 		void ClipSplit(IScriptEnvironment* env, PClip source, PClip& left, PClip& right) {
-			const char* arg_names[5] = { nullptr, nullptr, nullptr, nullptr, nullptr };
 			const VideoInfo &vi = source->GetVideoInfo();
-			AVSValue args[5] = { source, 0, 0, vi.width / 2, vi.height };
-			left = env->Invoke("crop", AVSValue(args, ARRAY_SIZE(args)), arg_names).AsClip();
-			args[1] = vi.width / 2;
-			right = env->Invoke("crop", AVSValue(args, ARRAY_SIZE(args)), arg_names).AsClip();
+			left  = AvsCall(env, "crop", source,            0, 0, vi.width / 2, vi.height).AsClip();
+			right = AvsCall(env, "crop", source, vi.width / 2, 0, vi.width / 2, vi.height).AsClip();
 			CheckVideoInfo(env, left->GetVideoInfo(), right->GetVideoInfo());
 		}
 
--- AvsTools/Tools.AviSynth.hpp
+++ AvsTools/Tools.AviSynth.hpp
@@ -1,23 +1,25 @@
+//
 // AviSynth useful C++ wrappers
 // Copyright 2016 Vyacheslav Napadovsky.
-//
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files(the "Software"), to deal
+// of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions :
-//
+// furnished to do so, subject to the following conditions:
+// 
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
 // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 
 #pragma once
 #include <AviSynth/avisynth.h>
@@ -31,20 +33,20 @@ namespace Tools {
 		// template that allows to call AviSynth functions easily
 		struct AvsNamedArg {
 			AVSValue value;
-			LPCSTR name;
-			AvsNamedArg(LPCSTR name, AVSValue value) : name(name), value(value) { }
+			const char *name;
+			AvsNamedArg(const char* name, AVSValue value) : name(name), value(value) { }
 			operator AVSValue() const { return value; }
 
-			template<typename T> static LPCSTR GetName(T v) {
+			template<typename T> static LPCSTR GetName(const T& v) {
 				return nullptr;
 			}
 		};
-		template<> inline LPCSTR AvsNamedArg::GetName(AvsNamedArg v) {
+		template<> inline LPCSTR AvsNamedArg::GetName(const AvsNamedArg& v) {
 			return v.name;
 		}
 
 		template<typename ... Args>
-		AVSValue AvsCall(IScriptEnvironment* env, const char* name, Args ... args) {
+		AVSValue AvsCall(IScriptEnvironment* env, const char* name, const Args& ... args) {
 			LPCSTR arg_names[sizeof...(Args)] = { AvsNamedArg::GetName(args) ... };
 			AVSValue arg_values[sizeof...(Args)] = { args ... };
 			return env->Invoke(name, AVSValue(arg_values, sizeof...(Args)), arg_names);
